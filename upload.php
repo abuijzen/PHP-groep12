6f9c2e39ff9a45db6b40c4aa782fb27cea06b494
Angelique Buijzen <angeliquebuijzen@hotmail.com>
2019-04-08 16:09:16 +0200

    styling form

--- upload.php
+++ upload.php
@@ -14,7 +14,7 @@
                 <input type="file" name="image">
             </div>
             <div>
-                <textarea name="description" cols="40" rows="4" placeholder="Wat wil je zeggen over deze foto?"><textarea>
+                <textarea name="description" cols="40" rows="4" placeholder="Wat wil je zeggen over deze foto?"></textarea>
             </div>
             <div>
                 <input type="submit" name="upload" value = "uploaden">
@@ -37,6 +37,25 @@ form{
 
 form div{
     margin-top:5px;
+}
+
+#img div{
+    width:80%;
+    padding:5px;
+    margin: 15px auto;
+    border: 1px solid #cbcbcb;
+}
+
+#img_div:after{
+    content:"";
+    display:block;
+    clear:both;
+}
+img{
+    float:left;
+    margin:5px;
+    width:300px;
+    height:140px;
 }
 
 </style>
