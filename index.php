600021b2a2b245f5af1e153e4c0989bc1896ac40
abuijzen <33352901+abuijzen@users.noreply.github.com>
2019-04-09 16:59:31 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require('upload.php');
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inspiration Hunter</title>
+</head>
+<body>
+<h1>Inspiration Hunter</h1>
+</body>
+</html>
