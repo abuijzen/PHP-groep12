de6f098111224eab58af3455e6a49b29adb5271b
abuijzen <33352901+abuijzen@users.noreply.github.com>
2019-04-09 17:35:33 +0200

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,5 +1,9 @@
 <nav>
-    <a href="index.php"><img src="https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiCitXGqcPhAhUSK1AKHQG7CB8QjRx6BAgBEAU&url=https%3A%2F%2Ftheinspirationhunter.podbean.com%2F&psig=AOvVaw2o5oukIsaohEekHcxVUCbS&ust=1554910072695083" alt="logo" width="150"></a>
+    <a href="index.php"><https://theinspirationhunter.podbean.com/mf/web/taf4uk/final-logo640240.jpg" alt="logo" width="150"></a>
     <a href="login.php">login</a>
     <a href="upload.php">Upload image</a>
 </nav>
+<style>
+    a{
+        margin:20px;
+</style>
