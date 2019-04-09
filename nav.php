3672db6b68d384b43dd3c3877cc16b9de98dd481
abuijzen <33352901+abuijzen@users.noreply.github.com>
2019-04-09 17:36:57 +0200

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,5 +1,5 @@
 <nav>
-    <a href="index.php"><https://theinspirationhunter.podbean.com/mf/web/taf4uk/final-logo640240.jpg" alt="logo" width="150"></a>
+    <a href="index.php"><img src="https://theinspirationhunter.podbean.com/mf/web/taf4uk/final-logo640240.jpg" alt="logo" width="150"></a>
     <a href="login.php">login</a>
     <a href="upload.php">Upload image</a>
 </nav>
