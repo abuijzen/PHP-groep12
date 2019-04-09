0f511c6a04ea88bb8bc068f49f54446323d7d280
abuijzen <33352901+abuijzen@users.noreply.github.com>
2019-04-09 17:29:20 +0200

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,5 @@
+<nav>
+    <a href="index.php"><img src="https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiCitXGqcPhAhUSK1AKHQG7CB8QjRx6BAgBEAU&url=https%3A%2F%2Ftheinspirationhunter.podbean.com%2F&psig=AOvVaw2o5oukIsaohEekHcxVUCbS&ust=1554910072695083" alt="logo" width="150"></a>
+    <a href="login.php">login</a>
+    <a href="upload.php">Upload image</a>
+</nav>
