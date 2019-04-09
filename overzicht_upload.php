07ed467fa0cc4ebd51862241da914edcc51e21d1
Angelique Buijzen <angeliquebuijzen@hotmail.com>
2019-04-09 20:57:48 +0200

    afbeelding formaat gelijk

--- overzicht_upload.php
+++ overzicht_upload.php
@@ -1,14 +1,22 @@
 <?php
+
 $conn = new PDO("mysql:host=localhost;dbname=inspiration_hunter","root","root",null);
-$result = $conn->query("SELECT * FROM tl_picture");
-    
-    if(!empty($result)){
-        $imgData = $result->fetchAll();
-        
-        //Render image
-        header("Content-type: image/jpg"); 
-        echo $imgData['image']; 
-    }else{
-        echo 'Image not found...';
-    }
-?>
+$statement = $conn->prepare("SELECT * FROM tl_picture");
+$statement->execute();
+$collection = $statement->fetchAll();
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php foreach($collection as $c): ?>
+<a href="detail-img.php"><img src="<?php echo $c['image']; ?>" alt="" height="200" width="200" style="object-fit: cover"></a>
+<p><?php echo $c['text']; ?></p>
+<?php endforeach; ?>
+</body>
+</html>
+
