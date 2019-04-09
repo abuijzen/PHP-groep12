2b491cd5fd620dc9463407d2d236a786f35b1b7c
Angelique Buijzen <angeliquebuijzen@hotmail.com>
2019-04-09 19:31:21 +0200

    overzicht toegevoegd

--- nav.php
+++ nav.php
@@ -2,6 +2,7 @@
     <a href="index.php"><b>Home<b></a>
     <a href="login.php">login</a>
     <a href="upload.php">Upload image</a>
+    <a href="overzicht_upload.php">image overzicht</a>
 </nav>
 <style>
     a{
