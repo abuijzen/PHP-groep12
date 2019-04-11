87c4b9ba09b2f101ce3c1affbeb38ca527fd2976
Amelie Mathijs <ameliemathijs1998@gmail.com>
2019-04-11 12:46:12 +0200

    poging tot veilige login maar werkt nog niet

--- login.php
+++ login.php
@@ -1,4 +1,35 @@
-<!DOCTYPE html>
+<?php
+if(!empty($_POST)){
+	//email en password opvragen
+	$email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $hashed_password = password_hash($_POST["password"],PASSWORD_DEFAULT);
+
+    //db connectie
+    $conn = new PDO('mysql:host=localhost;dbname=netflix;', "root", "root", null);
+
+    //email zoeken in db
+    $statement = $conn->prepare("select * from users where email = :email");
+	$statement->bindParam(":email", $email);
+    $result = $statement->execute();
+    $user = $statement->fetch(PDO::FETCH_ASSOC);
+
+    //passwoord en email komen overeen?
+    if(password_verify($password,$hashed_password)){
+        //ja -> naar index 
+		echo "joepie de poepie!!!!";
+		//session_start();
+		//$_SESSION['userid'] = $user['id'];
+
+		//header('location: index.php');
+	} else{
+		//nee -> error
+        echo "jammer joh";
+        //$error = true;
+    }
+}
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,21 +41,29 @@
     <?php include_once("nav.php"); ?>
 <h1>Inspiration Hunter</h1>
      
-<div class="form form--login">
+<div class="form">
             <form action="" method="post">
                 <h2 form__title>Sign In</h2>
- 
+
+                <?php if (isset($error)): ?>
+				<div class="form__error">
+					<p>
+						Sorry, we can't log you in with that email address and password. Can you try again?
+					</p>
+				</div>
+                <?php endif; ?>
+                
                 <div class="form__field">
-                    <label for="Email" >Email</label>
-                    <input type="text" name="Email" id="Email">
+                    <label for="email" >Email</label>
+                    <input type="text" name="email" id="email">
                 </div>
                 <div class="form__field">
-                    <label for="Password">Password</label>
-                    <input type="password" id="Password" name="Password">
+                    <label for="password">Password</label>
+                    <input type="password" id="password" name="password">
                 </div>
 
                 <div class="form__field">
-                    <input type="submit" value="Sign in" class="btn btn--primary"> 
+                    <input type="submit" value="Sign in" class="btn"> 
                 </div>      
             </form>
         </div>
