32fd12d2bb9488924bd111470d7e44d2a18feeef
abuijzen <33352901+abuijzen@users.noreply.github.com>
2019-04-12 15:21:44 +0200

    classe post

--- upload.php
+++ upload.php
@@ -1,24 +1,15 @@
 <?php
-
+//link naar classe Post
+require_once("classes/post.class.php"); 
 //upload moet iets bevatten
 if(!empty($_FILES['image']['name'])){
-
 // pad waar afbeelding wordt opgeslagen
 $target= "images/".basename($_FILES['image']['name']);
-
-//krijg data die gesubmitted is
-$image = $_FILES['image']['name'];
-$text = (htmlspecialchars($_POST['text']));
-
-$conn = new PDO("mysql:host=localhost;dbname=inspiration_hunter","root","root",null);
-$insert = $conn->prepare("INSERT INTO tl_picture(image, text) VALUES (:image, :text)");
-try{
-    if(!$insert->execute(array(':image' => $image, ':text' => $text)))
-        die("Unknown ERROR!");
-} catch(PDOException $ex) {
-    die($e->getMessage());
-}
-
+//new post maken
+$post = new Post();
+$post->setImage($_FILES['image']['name']);
+$post->setText(htmlspecialchars($_POST['text']));
+$post->getSubmittedPosts();
 //zet de geüploadede afbeelding in de map "images"
 if (move_uploaded_file($_FILES['image']['tmp_name'],$target)){
     echo"afbeelding is opgeslagen";
@@ -46,7 +37,6 @@ if (move_uploaded_file($_FILES['image']['tmp_name'],$target)){
         <!--zonder enctype kan je geen file uploaden-->
         <form method="post" action ="upload.php" enctype="multipart/form-data">
         
-
             <input type="hidden" name="size" value="100000">
             <div>
                 <!--HTML5 code die ervoor zorgt dat je op je gsm rechtstreeks een foto kan maken-->
@@ -79,8 +69,6 @@ $collection = $statement->fetchAll();
 
 
 <style>
-
-
 form{
     width:100%;
     padding:30px;
@@ -93,17 +81,14 @@ form{
     flex-wrap:wrap;
     justify-content:left;
 }
-
 .post p{
 font-weight:100;
 font-family:sans-serif;
 margin-left:25px;
 }
-
 img{
     margin:5px;
 }
-
 </style>
 
 </body>
