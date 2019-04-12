97bec3e96abcfe76292ded5cdc70931576c19413
Angelique Buijzen <angeliquebuijzen@hotmail.com>
2019-04-12 10:50:49 +0200

    overzicht verwijderd

--- nav.php
+++ nav.php
@@ -3,7 +3,6 @@
     <a href="login.php">Login</a>
     <a href="register.php">Register</a>
     <a href="upload.php">Upload image</a>
-    <a href="overzicht_upload.php">Image overzicht</a>
     <a href="logout.php">Log out</a>
 </nav>
 <style>
