4080f25e337ded457789b2009b99824df1bdddd1
Angelique Buijzen <angeliquebuijzen@hotmail.com>
2019-04-12 22:17:05 +0200

    full view CHECK

--- detail-img.php
+++ detail-img.php
@@ -1,15 +1,11 @@
 <?php 
 
-
-
 if (!empty($_GET["id"])) {
   $id = ($_GET["id"]);
   $conn = new PDO("mysql:host=localhost;dbname=inspiration_hunter","root","root",null);
-  $selectId = $conn->prepare("SELECT * FROM tl_picture where id='84'");
+  $selectId = $conn->prepare("SELECT * FROM tl_picture where id='$id'");
   $selectId->execute();
-  $selectId = $selectId->fetchAll(PDO::FETCH_OBJ);
-  var_dump($selectId);
-  echo ("<br><br>ID die wordt meegegeven met de url: ".$id."<br>");
+  $selectId = $selectId->fetchAll(PDO::FETCH_ASSOC);
 } 
 
 ?><!DOCTYPE html>
@@ -23,10 +19,21 @@ if (!empty($_GET["id"])) {
   
   <?php include_once("nav.php"); ?>
   
-  
-  <img src="images/<?php echo $selectId[$id]['image']; ?>" alt="" height="200" width="200" style="object-fit: cover"></a>
-  <p><?php echo $selectId[$id]['text'] ?></p>
+  <img src="images/<?php echo $selectId[0]['image']; ?>" alt="" height="auto" width="50%" style="object-fit: cover"></a>
+  <p><?php echo $selectId[0]['text'] ?></p>
 
+<style>
+
+p{
+  font-family:sans-serif;
+  font-weight:100;
+  margin-left:40px;
+}
+img{
+  margin:40px 0px 0px 40px;
+  
+}
+</style>
 
 </body>
 </html>
