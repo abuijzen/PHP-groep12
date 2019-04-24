89a6a9fd4c2f5be6311f6277e426872c492417bd
Ruben Annaert <rubenannaert1998@gmail.com>
2019-04-25 00:25:01 +0200

    aanpassingen voor feature 8

--- index.php
+++ index.php
@@ -9,7 +9,10 @@
 <body>
     <?php include_once("nav.php"); ?>
 <h1>Inspiration Hunter</h1>
-     
-   
+    <article>
+		<p></p>
+		<img src="http://dummyimage.com/300x200/5921E8/fff" alt="">
+		<div><a href="#" class="like">Like</a> <span class='likes'>xxx</span> people like this </div>
+	</article>
 </body>
 </html>
