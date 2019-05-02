dc0561090eab42e025d305eb698966161bdd79ca
abuijzen <33352901+abuijzen@users.noreply.github.com>
2019-05-02 12:04:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,22 @@
 <?php
-$conn = new PDO("mysql:host=localhost;dbname=inspiration_hunter","root","root",null);
+$conn = new PDO('mysql:host=localhost;dbname=eurben', 'root', 'root', null);
 
-
-if(!empty($_GET['search'])){
-    $innerhtml= $_GET['search']; 
-}
-
-else{
-    $innerhtml = "";
+if (!empty($_GET['search'])) {
+    $innerhtml = $_GET['search'];
+} else {
+    $innerhtml = '';
 }
 
 // Alle resultaten
-$alleResultaten = $conn->prepare("SELECT*FROM tl_picture WHERE text LIKE '%$innerhtml%' ORDER BY id DESC");
+$alleResultaten = $conn->prepare("SELECT*FROM post WHERE message LIKE '%$innerhtml%' ORDER BY id DESC");
 $alleResultaten->execute();
-$countAll =$alleResultaten->rowCount();
-
+$countAll = $alleResultaten->rowCount();
 
 //zichtbare resultaten
-$statement = $conn->prepare("SELECT*FROM tl_picture WHERE text LIKE '%$innerhtml%' ORDER BY id DESC  limit 20");
+$statement = $conn->prepare("SELECT*FROM post WHERE message LIKE '%$innerhtml%' ORDER BY id DESC  limit 20");
 $statement->execute();
 $collection = $statement->fetchAll();
-$count =$statement->rowCount();
-
+$count = $statement->rowCount();
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -33,75 +28,109 @@ $count =$statement->rowCount();
 </head>
 <body>
 <h1>Inspiration Hunter</h1>
-    <?php include_once("nav.php"); 
-    
-    echo "<br>Founded results: ".$countAll."<br>";
-    
-    echo "viewable results: ". $count;
+    <?php include_once 'nav.php';
+
+    echo '<br>Founded results: '.$countAll.'<br>';
+
+    echo 'viewable results: '.$count;
     ?>
 
     <div class="all-posts">
 
 <!--indien er GEEN resultaten worden gevonden-->
-<?php if($count==0): ?>
+<?php if ($count == 0): ?>
 <p>Geen resultaten gevonden<p>
 <?php endif; ?>
 
 <!--indien er WEL resultaten worden gevonden-->
-<?php if($count>=1): ?>
-<?php foreach($collection as $c): ?>
+<?php if ($count >= 1): ?>
+<?php foreach ($collection as $c): ?>
 <div class="post">
 <a href="detail-img.php?id=<?php echo $c['id']; ?>"><img src="images/<?php echo $c['image']; ?>" alt="" height="200" width="200" style="object-fit: cover"></a>
-<p><?php echo $c['text']; ?></p>
+<p><?php echo $c['message']; ?></p>
+<br>
+<?php date_default_timezone_set('Europe/Brussels');
 
-<?php 
+// datum vandaag
+$vandaag = date('Y-m-d');
 
-echo date("Y-m-d h:i:s ")."tijd nu<br>";
-echo $c['time']." tijd upload<br>";
+// datum + tijd vandaag
+$vandaagDateTime = date('Y-m-d H:i:s ');
 
-if(date("Y-m-d")>$c['time']){
-    echo "yesterday ". date('H i',strtotime($c['time']));
+//net gepost
+$nuTime = date('H:i:s', strtotime('-1 sec'));
+
+// datum gisteren
+$yesterday = date('Y-m-d', strtotime('-1 days'));
+
+// vanaf meer dan 1 dag geleden
+$eergisteren = date('Y-m-d', strtotime('-2 days'));
+
+// datum + tijd van de upload
+$uploadDateTime = date('Y-m-d H:i', strtotime($c['timePost']));
+
+// datum van de upload
+$uploadTime = date('H:i', strtotime($c['timePost']));
+
+// datum van de upload
+$uploadDate = date('Y-m-d', strtotime($c['timePost']));
+
+//half uur geleden
+$halfHourAgo = date('H:i', strtotime('-30 min'));
+
+//15 min geleden
+$quarterAgo = date('H:i', strtotime('-15 min'));
+
+//een uur geleden
+$hourAgo = date('H:i', strtotime('-1 hour'));
+
+//2 uur geleden
+//$twoHoursAgo =date('H:i',strtotime("-2 hour"));
+
+// Is de post gisteren geplaatst?
+if ($uploadDate == $yesterday) {
+    echo 'gisteren gepost om: '.date('H:i', strtotime($uploadTime)).'<br>';
 }
 
-
-
-//datum van vandaag is kleiner dan gisteren
-/*if(date($c['time'])<date("F j Y g:i a")){
-    echo "today";
+//eergisteren gepost?
+if ($uploadDate <= $eergisteren) {
+    echo $uploadDateTime;
 }
 
+// ---------indien vandaag gepost: meerdere opties van mededelingen ----------
+switch ($uploadDate == $vandaag) {
+//meer dan een uur geleden gepost
+case $uploadTime < $hourAgo:
+    echo'<br>1 uur geleden<br>';
+break;
 
-if(date('d M',time($c['time']."-1 days"))<date('d M ', time($c['time']))){
-    echo "today";
+//meer dan een half uur geleden
+case $uploadTime < $halfHourAgo:
+    echo '<br>Half uur geleden<br>';
+break;
+
+//minder dan een kwartier
+case $uploadTime < $nuTime:
+    echo '<br>Zonet<br>';
 }
 
-//kleinere datum dan vandaag
-if(date('Y-m-d', time($c['time']))<date('Y-m-d')){
-    echo "yesterday";
-}
-*/
-//echo gisteren
-//echo date('d M',strtotime($c['time']."-1 days"));
-//echo date("F j, Y, g:i a");?>
-<div><a href="#" class="like">Like</a> <span class='likes'>xxx</span> people like this </div>
-</div>
+?>
+<div>
+<a href="#" class="like">Like</a><span class='likes'>xxx</span> people like this </div>
+
 <?php endforeach; ?>
 <?php endif; ?>
 
 </div>
 
 <?php
-$conn = new PDO("mysql:host=localhost;dbname=inspiration_hunter","root","root",null);
+$conn = new PDO('mysql:host=localhost;dbname=eurben', 'root', 'root', null);
 
-
-if(!empty($_GET['search'])){
-    $innerhtml= $_GET['search']; 
+if (!empty($_GET['search'])) {
+    $innerhtml = $_GET['search'];
+} else {
+    $innerhtml = '';
 }
-
-else{
-    $innerhtml = "";
-}
-
 
 ?>
 
