9bf643b7b6c0492d827024f57fc1410ad6400caf
Ruben Annaert <rubenannaert1998@gmail.com>
2019-05-02 22:12:03 +0200

    Iets dynamischer autoloading function

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,4 @@
 <?php
     spl_autoload_register(function ($class) {
-        include 'classes/'.$class.'.class.php';
+        require_once __DIR__.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR.$class.'.class.php';
     });
