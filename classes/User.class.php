2a570eff40990ecf56ed3131152577ab96d6d90a
Ruben Annaert <rubenannaert1998@gmail.com>
2019-05-02 23:17:14 +0200

    PDO connectie vervangen door Db::getInstance();

--- classes/User.class.php
+++ classes/User.class.php
@@ -1,92 +1,90 @@
 <?php
-    class User{
+    class User
+    {
         private $email;
         private $password;
         private $passwordConfirmation;
 
-
-
         /**
-         * Get the value of email
-         */ 
+         * Get the value of email.
+         */
         public function getEmail()
         {
-                return $this->email;
+            return $this->email;
         }
 
         /**
-         * Set the value of email
+         * Set the value of email.
          *
-         * @return  self
-         */ 
+         * @return self
+         */
         public function setEmail($email)
         {
-                $this->email = $email;
+            $this->email = $email;
 
-                return $this;
+            return $this;
         }
 
         /**
-         * Get the value of password
-         */ 
+         * Get the value of password.
+         */
         public function getPassword()
         {
-                return $this->password;
+            return $this->password;
         }
 
         /**
-         * Set the value of password
+         * Set the value of password.
          *
-         * @return  self
-         */ 
+         * @return self
+         */
         public function setPassword($password)
         {
-                $this->password = $password;
+            $this->password = $password;
 
-                return $this;
+            return $this;
         }
 
         /**
-         * Get the value of passwordConfirmation
-         */ 
+         * Get the value of passwordConfirmation.
+         */
         public function getPasswordConfirmation()
         {
-                return $this->passwordConfirmation;
+            return $this->passwordConfirmation;
         }
 
         /**
-         * Set the value of passwordConfirmation
+         * Set the value of passwordConfirmation.
          *
-         * @return  self
-         */ 
-        
+         * @return self
+         */
         public function setPasswordConfirmation($passwordConfirmation)
         {
-                $this->passwordConfirmation = $passwordConfirmation;
+            $this->passwordConfirmation = $passwordConfirmation;
 
-                return $this;
+            return $this;
         }
 
         //@todo: form validation
-        public function register(){
-                $options = [
-                        'cost' => 14 //2^14
+        public function register()
+        {
+            $options = [
+                        'cost' => 14, //2^14
                 ];
-                $password = password_hash($this->password,PASSWORD_DEFAULT,$options);
-                
-                try{
-                        //alles wat je wil proberen
-                        //$conn = new PDO("mysql:host=localhost;dbname=netflix","root","root",null); indien hij een 4e vraagt
-                        $conn = new PDO("mysql:host=localhost;dbname=netflix","root","root");
-                        $statement = $conn->prepare("INSERT into users(email,password) VALUES (:email,:password)");
-                        $statement->bindParam(":email",$this->email);
-                        $statement->bindParam(":password",$password);
-                        $result = $statement->execute();
-                        return $result;
-                }catch(Throwable $t){
-                        return false;
-                } 
+            $password = password_hash($this->password, PASSWORD_DEFAULT, $options);
 
+            try {
+                //alles wat je wil proberen
+                //$conn = new PDO("mysql:host=localhost;dbname=netflix","root","root",null); indien hij een 4e vraagt
+                $conn = Db::getInstance();
+                $statement = $conn->prepare('INSERT into users(email,password) VALUES (:email,:password)');
+                $statement->bindParam(':email', $this->email);
+                $statement->bindParam(':password', $password);
+                $result = $statement->execute();
+
+                return $result;
+            } catch (Throwable $t) {
+                return false;
+            }
         }
     }
-
