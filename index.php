257ea22242a9034526164ac8118d008cca9b38bc
Angelique Buijzen <angeliquebuijzen@hotmail.com>
2019-05-08 19:37:53 +0200

    datum toegevoegd

--- index.php
+++ index.php
@@ -47,6 +47,72 @@
                     <div class="post">
                         <a href="detail_img.php?id=<?php echo $c['id']; ?>"><img src="images/<?php echo $c['image']; ?>" alt="" height="200" width="200" style="object-fit: cover"></a>
                         <p><?php echo $c['message']; ?></p>
+                        <?php date_default_timezone_set('Europe/Brussels');
+
+// datum vandaag
+$vandaag = date('Y-m-d');
+
+// datum + tijd vandaag
+$vandaagDateTime = date('Y-m-d H:i:s ');
+
+//net gepost
+$nuTime = date('H:i:s', strtotime('-1 sec'));
+
+// datum gisteren
+$yesterday = date('Y-m-d', strtotime('-1 days'));
+
+// vanaf meer dan 1 dag geleden
+$eergisteren = date('Y-m-d', strtotime('-2 days'));
+
+// datum + tijd van de upload
+$uploadDateTime = date('Y-m-d H:i', strtotime($c['timePost']));
+
+// datum van de upload
+$uploadTime = date('H:i', strtotime($c['timePost']));
+
+// datum van de upload
+$uploadDate = date('Y-m-d', strtotime($c['timePost']));
+
+//half uur geleden
+$halfHourAgo = date('H:i', strtotime('-30 min'));
+
+//15 min geleden
+$quarterAgo = date('H:i', strtotime('-15 min'));
+
+//een uur geleden
+$hourAgo = date('H:i', strtotime('-1 hour'));
+
+//2 uur geleden
+//$twoHoursAgo =date('H:i',strtotime("-2 hour"));
+
+// Is de post gisteren geplaatst?
+if ($uploadDate == $yesterday) {
+    echo 'gisteren gepost om: '.date('H:i', strtotime($uploadTime)).'<br>';
+}
+
+//eergisteren gepost?
+if ($uploadDate <= $eergisteren) {
+    echo $uploadDateTime;
+}
+
+// ---------indien vandaag gepost: meerdere opties van mededelingen ----------
+switch ($uploadDate == $vandaag) {
+//meer dan een uur geleden gepost
+case $uploadTime < $hourAgo:
+    echo'<br>1 uur geleden<br>';
+break;
+
+//meer dan een half uur geleden
+case $uploadTime < $halfHourAgo:
+    echo '<br>Half uur geleden<br>';
+break;
+
+//minder dan een kwartier
+case $uploadTime < $nuTime:
+    echo '<br>Zonet<br>';
+}
+
+?>
                         <br>
                         <div><a href="#" class="likes">Like</a><span class='likes'>xxx</span> people like this </div>
                         </div>
