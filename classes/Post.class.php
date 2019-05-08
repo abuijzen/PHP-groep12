b87c781800baa6177f99ee933e4d5289afaee64b
Ruben Annaert <rubenannaert1998@gmail.com>
2019-05-09 01:42:49 +0200

    post klasse getLikes toegevoegt

--- classes/Post.class.php
+++ classes/Post.class.php
@@ -140,4 +140,24 @@
                 return $nothing;
             }
         }
+
+        public static function getAll()
+        {
+            $conn = Db::getInstance();
+            $result = $conn->query('select * from posts ');
+
+            // fetch all records from the database and return them as objects of this __CLASS__ (Post)
+            return $result->fetchAll(PDO::FETCH_CLASS, __CLASS__);
+        }
+
+        public function getLikes()
+        {
+            $conn = Db::getInstance();
+            $statement = $conn->prepare('select count(*) as count from likes where post_id = :postid AND user_id=:user_id');
+            $statement->bindValue(':postid', $this->id);
+            $statement->execute();
+            $result = $statement->fetch(PDO::FETCH_ASSOC);
+
+            return $result['count'];
+        }
     }
