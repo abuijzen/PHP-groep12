858c378153476981d01b0aee75b0043fd3347daa
Ruben Annaert <rubenannaert1998@gmail.com>
2019-05-10 00:28:05 +0200

    fail res.status toegevoegd om like te verwijderen

--- js/like.js
+++ js/like.js
@@ -1,25 +1,26 @@
 
-	$("a.likes").on("click", function(e){
-		var postsId = $(this).data('id');
-		var elLikes = $(this).parent().find(".likes");
-		var likes = elLikes.html();
+$("a.likes").on("click", function (e) {
+	var postsId = $(this).data('id');
+	var elLikes = $(this).parent().find(".likesAmount");
+	var likes = elLikes.text();
 
-		$.ajax({
-			method: "POST",
-			url: "ajax/like.php",
-			data: { postsId: postsId },
-			dataType: "json"
-		})
-
-		.done(function( res ) {
-			// console.log("test 123");
-
-			if(res.status == "success"){
-				likes++;
-				elLikes.html(likes);
-			}
-			// console.log(res);
-		});
-
-		e.preventDefault();
+	$.ajax({
+		method: "POST",
+		url: "ajax/like.php",
+		data: { postsId: postsId },
+		dataType: "json"
+	}).done(function (res) {
+		// console.log("test 123");
+		console.log(res);
+		if (res.status == "success") {
+			likes++;
+			elLikes.text(likes);
+		} else if (res.status == "fail") {
+			likes--;
+			elLikes.text(likes);
+		}
+		// console.log(res);
 	});
+
+	e.preventDefault();
+}); 
