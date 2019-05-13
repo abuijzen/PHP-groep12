85fbff059b959e055f6d0eef969238fb3c116c34
Angelique Buijzen <angeliquebuijzen@hotmail.com>
2019-05-13 15:43:11 +0200

    checken op bestandgrootte en type

--- upload.php
+++ upload.php
@@ -7,27 +7,76 @@ if (isset($_SESSION['email'])) {
 } else {
     header('location:login.php');
 }
+
 $post = new Post();
 
-//upload moet iets bevatten
+$errors = array();
 if (!empty($_FILES['image']['name'])) {
-    // pad waar afbeelding wordt opgeslagen
-    $target = 'images/'.basename($_FILES['image']['name']);
-
-    //new post maken
     $post->setImage($_FILES['image']['name']);
     $post->setText(htmlspecialchars($_POST['text']));
     $post->setFilter(htmlspecialchars($_POST['filter']));
     $post->uploadPosts();
+    //get provided file information
+    $fileName = $_FILES['image']['name'];
+    $fileExtArr = explode('.', $fileName); //make array of file.name.ext as    array(file,name,ext)
+        $fileExt = strtolower(end($fileExtArr)); //get last item of array of user file input
+        $fileSize = $_FILES['image']['size'];
+    $fileTmp = $_FILES['image']['tmp_name'];
 
-    //zet de geüploadede afbeelding in de map "images"
-    if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
-        echo'afbeelding is opgeslagen';
+    //which files we accept
+    $allowed_files = ['jpg', 'png', 'gif'];
+
+    //validate file size
+    if ($fileSize > (1024 * 1024 * 2)) {
+        $errors[] = 'Maximum 2MB files are allowed';
+    }
+
+    //validating file extension
+    if (!in_array($fileExt, $allowed_files)) {
+        $errors[] = 'only ('.implode(', ', $allowed_files).') files are allowed.';
+    }
+
+    //do other validations here if you need more
+
+    //error array moet leeg zijn
+    if (empty($errors)) {
+        move_uploaded_file($fileTmp, 'images/'.$fileName);
+        echo 'File uploaded successfully.';
         header('Location:index.php');
     } else {
-        echo'afbeelding is niet opgeslagen';
+        echo 'Some Error Occured: <br>'.implode('<br>', $errors);
     }
+} else {
+    $errors[] = 'No Image is provided.';
 }
+
+/*//function to create thumbnail
+create_thumb($target,$ext,$thumb_path,$w,$h){
+        list($w_orig,$h_orig)=getimagesize($target);
+        $scale_ratio=$w_orig/$h_orig;
+        if(($w/$h)>$scale_ratio)
+            $w=$h*$scale_ratio;
+        else
+            $h=$w/$scale_ratio;
+
+    if($w_orig<=$w){
+        $w=$w_orig;
+        $h=$h_orig;
+    }
+    $img="";
+    if($ext=="gif")
+        $img=imagecreatefromgif($target);
+    else if($ext=="png")
+        $img=imagecreatefrompng($target);
+    else if($ext=="jpg")
+        $img=imagecreatefromjpeg($target);
+
+    $tci=imagecreatetruecolor($w,$h);
+    imagecopyresampled($tci,$img,0,0,0,0,$w,$h,$w_orig,$h_orig);
+    imagejpeg($tci,$thumb_path,80);
+    imagedestroy($tci);
+}
+*/
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -39,8 +88,7 @@ if (!empty($_FILES['image']['name'])) {
     <link rel="stylesheet" href="css/screen.css">
 </head>
 <body>
-<!--ajax inladen-->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
 <!--navigatie inladen-->
     <?php include_once 'nav.php'; ?> 
     <h2>Upload image</h2>
