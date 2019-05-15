a07e0621a4b90db4a617f989f8f90138b8cafc4b
Sisiboi <36501432+Sisiboi@users.noreply.github.com>
2019-05-15 15:03:05 +0200

    Profile page
    pagina voor profiel te bekijken

---
+++ profile.php
@@ -0,0 +1,106 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>Inspiration Hunter</title>
+        <!--     Fonts and icons     -->
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"
+        />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+        <!-- CSS Files -->
+        <link href="css/material-kit.css?v=2.0.5" rel="stylesheet" />
+</head>
+
+<?php include_once 'nav.php'; ?>
+<body class="profile-page sidebar-collapse">
+ 
+  <div class="page-header" ></div>
+  <div class="main main-raised">
+    <div class="profile-content">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 ml-auto mr-auto">
+            <div class="profile">
+              <div class="avatar">
+                <img src="https://pixel.nymag.com/imgs/daily/vulture/2018/09/23/29-Lindsay-Lohan.w330.h330.jpg" alt="Circle Image" class="img-raised rounded-circle img-fluid">
+              </div>
+              <div class="name">
+                <h3 class="title">Christian Louboutin</h3>
+                <h6>Designer</h6>
+              
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="description text-center">
+          <p>An artist of considerable range, Chet Faker &#x2014; the name taken by Melbourne-raised, Brooklyn-based Nick Murphy &#x2014; writes, performs and records all of his own music, giving it a warm, intimate feel with a solid groove structure. </p>
+        </div>
+        <div class="row">
+          <div class="col-md-6 ml-auto mr-auto">
+            <div class="profile-tabs">
+              <ul class="nav nav-pills nav-pills-icons justify-content-center" role="tablist">
+                <li class="nav-item">
+                  <a class="nav-link active" href="#studio" role="tab" data-toggle="tab">
+                    <i class="material-icons">camera</i> Studio
+                  </a>
+                </li>
+              
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="tab-content tab-space">
+          <div class="tab-pane active text-center gallery" id="studio">
+            <div class="row">
+              <div class="col-md-3 ml-auto">
+                <img src="../assets/img/examples/studio-1.jpg" class="rounded">
+                <img src="../assets/img/examples/studio-2.jpg" class="rounded">
+              </div>
+              <div class="col-md-3 mr-auto">
+                <img src="../assets/img/examples/studio-5.jpg" class="rounded">
+                <img src="../assets/img/examples/studio-4.jpg" class="rounded">
+              </div>
+            </div>
+          </div>
+          <div class="tab-pane text-center gallery" id="works">
+            <div class="row">
+              <div class="col-md-3 ml-auto">
+                <img src="../assets/img/examples/olu-eletu.jpg" class="rounded">
+                <img src="../assets/img/examples/clem-onojeghuo.jpg" class="rounded">
+                <img src="../assets/img/examples/cynthia-del-rio.jpg" class="rounded">
+              </div>
+              <div class="col-md-3 mr-auto">
+                <img src="../assets/img/examples/mariya-georgieva.jpg" class="rounded">
+                <img src="../assets/img/examples/clem-onojegaw.jpg" class="rounded">
+              </div>
+            </div>
+          </div>
+          <div class="tab-pane text-center gallery" id="favorite">
+            <div class="row">
+              <div class="col-md-3 ml-auto">
+                <img src="../assets/img/examples/mariya-georgieva.jpg" class="rounded">
+                <img src="../assets/img/examples/studio-3.jpg" class="rounded">
+              </div>
+              <div class="col-md-3 mr-auto">
+                <img src="../assets/img/examples/clem-onojeghuo.jpg" class="rounded">
+                <img src="../assets/img/examples/olu-eletu.jpg" class="rounded">
+                <img src="../assets/img/examples/studio-1.jpg" class="rounded">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+ 
+  
+</body>
+
+</html>
