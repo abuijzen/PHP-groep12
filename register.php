c0152ae3040d4762150d24bc61e5864d045663c0
Ruben Annaert <rubenannaert1998@gmail.com>
2019-05-18 23:00:22 +0200

    Update register.php
    Mooi maken van code + aanpassing link

--- register.php
+++ register.php
@@ -49,20 +49,13 @@
 
     <div class="page-header header-filter" style="background-image: url('https://images.unsplash.com/photo-1535375743084-67f559ec192f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'); background-size: cover; background-position: top center;">
       <div class="container">
-
-
         <div class="row">
           <div class="col-lg-4 col-md-6 ml-auto mr-auto">
             <div class="card card-login">
               <form class="form" action="" method="post">
-
-
-
                 <div class="card-header card-header-primary text-center">
                   <h2 class="card-title">Eurben</h2>
-
                   <h4 class="card-title">Register</h4>
-
                 </div>
                 <p class="description text-center">Enter your info below</p>
                 <div class="card-body">
@@ -76,8 +69,6 @@
                     </div>
                     <input type="text" name="firstname" id="firstname" class="form-control" placeholder="First name...">
                   </div>
-
-
                   <div class="input-group">
                     <div class="input-group-prepend">
                       <span class="input-group-text">
@@ -88,7 +79,6 @@
                     </div>
                     <input type="text" name="lastname" id="lastname" class="form-control" placeholder="Last name...">
                   </div>
-
                   <div class="input-group">
                     <div class="input-group-prepend">
                       <span class="input-group-text">
@@ -119,7 +109,6 @@
                   </div>
                 </div>
                 <div class="footer text-center">
-
                   <input type="submit" value="Sign in" class="btn btn-primary">
                   <?php if (isset($error)): ?>
                   <div class="form__error">
@@ -128,24 +117,12 @@
                     </p>
                   </div>
                   <?php endif; ?>
-
-
                 </div>
-
-
+                <a href="login.php">Already an account? Sign in here!</a>
               </form>
-
             </div>
-            <a href="register.php">Not an account yet? Sign up here!</a>
           </div>
-
         </div>
       </div>
-
-
-
-
-
   </body>
-
   </html>
